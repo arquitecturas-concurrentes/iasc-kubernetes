d1b4996abf86021c718e235b8bfa63d6080b3c0b
Ernesto Bossi <bossi.ernestog@gmail.com>
2022-11-03 15:36:46 -0300

    Update up.bash

--- 5-services/up.bash
+++ 5-services/up.bash
@@ -4,6 +4,7 @@ CLUSTER="iasc-example"
 SERVICE="iasc-service-example"
 CONTEXT="do-nyc1-${CLUSTER}"
 doctl k8s cluster create ${CLUSTER}
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.6.1/components.yaml
 kubectl --context ${CONTEXT} apply -f iasc-service-complete.yaml
 ./scripts/wait-for-service ${CONTEXT} ${SERVICE}
 open http://$(kubectl --context ${CONTEXT} get service ${SERVICE} --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
